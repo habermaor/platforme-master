7371c67bd53e5e7df1d87bfdddacf7c4ebbf6879
Maor <habermaor@gmail.com>
2017-12-27 11:02:07 +0200

    init

---
+++ src/app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var path = require('path');
+
+
+var app = express();
+
+
+
+var //editor = [express.static(path.resolve(__dirname, '../../platforme-editor/')), (req, res) => res.sendFile(path.resolve(__dirname, '../../platforme-editor/index.html'))],
+    player = [express.static(path.resolve(__dirname , '../../player')), (req, res) => res.sendFile(__dirname , '../../player/index.html')];
+
+
+
+app.get('/', player);
+//app.get('/editor', editor);
+app.use(player);
+
+
+
+app.listen(3000);
+
