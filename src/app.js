82bfbdde39ada5c661a76403fd47afa4a79173a4
Maor <habermaor@gmail.com>
2018-07-22 14:14:11 +0300

    working with player and editor

--- src/app.js
+++ src/app.js
@@ -1,17 +1,18 @@
 
 var express = require('express');
 var cors = require('cors');
-
+var bodyParser = require('body-parser')
 var path = require('path');
 var mysql = require('mysql');
 var app = express();
+app.use(bodyParser.json({ limit: '50mb' }));
 
-var player = [express.static(path.resolve(__dirname , '../../player')), (req, res) => res.sendFile(__dirname , '../../player/index.html')];
-
+var player = [express.static(path.resolve(__dirname, '../../player')),(req, res) => res.sendFile(__dirname, '../../player/index.html')];
+var editor = [express.static(path.resolve(__dirname, '../../editor')), (req, res) => res.sendFile(__dirname, '../../editor/index.html')];
 
 const api = express.Router();
 api.get('/item/:route', function (req, res) {
-    console.log(req, res, "in readGameRecord");
+
     pool = mysql.createPool({
         connectionLimit: 10,
         host: DB_CONFIG.HOST,
@@ -23,10 +24,9 @@ api.get('/item/:route', function (req, res) {
         // connected! (unless `err` is set)
     });
     pool.on('connection', function (connection) {
-        console.log("connected");
         connection.query({
             sql: 'SELECT * FROM `games` WHERE `route` = ?',
-            timeout: 2000, // 2s
+            timeout: 20000, // 20s
             values: [req.params.route]
         }, function (error, results, fields) {
             if (results) {
@@ -37,6 +37,35 @@ api.get('/item/:route', function (req, res) {
         })
     });
 });
+api.post('/create', function (req, res) {
+    console.log("req.body of create", req.body);
+    pool = mysql.createPool({
+        connectionLimit: 10,
+        host: DB_CONFIG.HOST,
+        user: DB_CONFIG.USERNAME,
+        password: DB_CONFIG.PASSWORD,
+        database: DB_CONFIG.DBNAME
+    });
+    pool.getConnection(function (err, connection) {
+        // connected! (unless `err` is set)
+    });
+    pool.on('connection', function (connection) {
+        console.log("connected in create item");
+        var game = {
+            author_id: 999, //TODO - use user id
+            route: Math.random().toString(36).substring(7), //TODO - check if exists..
+            value: JSON.stringify(req.body)
+        }
+        connection.query('INSERT INTO games SET ?', game, function (err, result) {
+            if (result) {
+                res.send(result)
+            }
+            else
+                console.log(err);
+        });
+    });
+
+});
 const DB_CONFIG = {
     "HOST": "platformedb.c9vmaegoysdi.us-east-2.rds.amazonaws.com",
     "USERNAME": "platformedb",
@@ -46,8 +75,8 @@ const DB_CONFIG = {
 
 app.use(cors());
 app.use('/api/', api);
-app.use('/office/', player);
-app.use('/jungle/', player);
+app.use('/editor/', editor);
+app.use('/player/', player);    
 app.use(player);
 module.exports = app;
 
